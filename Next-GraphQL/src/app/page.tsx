71ef5cbeb5251ea218fddc9b067c1d366350340d
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2025-01-09 21:15:14 +0500

    Final Commit

--- Next-GraphQL/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-const Home = async () => {
-    return (
-        <div className="flex flex-col justify-center items-center gap-4 h-screen">
-        </div>
-    )
-};
-
-export default Home;
---
+++ Next.JS-GraphQL/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Novels from "@/Components/Novels";
+
+const Home = async () => {
+    return <div className="mx-auto max-w-5xl"><Novels /></div>
+};
+
+export default Home;
